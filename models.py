189fbb26ebd13212ac6f51ab9b66b33aed55472a
tougshire <benjamin@tougshire.com>
2021-09-06 16:27:53 +0000

    Renamed tougdo_auth to tougauth

--- models.py
+++ models.py
@@ -2,29 +2,29 @@ from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser, Group
 )
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import gettext_lazy as _
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, username, password=None):
         """
         Creates and saves a User with the given emai and password.
         """
-        if not email:
-            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError('Users must have an username')
 
-        user = self.model(
-            email=self.normalize_email(email),
-        )
+        user = self.model( username=username )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, username, password=None):
         """
         Creates and saves a superuser with the given email and password.
         """
         user = self.create_user(
-            email,
+            username,
             password=password,
         )
         user.is_admin = True
@@ -35,6 +35,22 @@ class Group(Group):
 	group_email = models.EmailField()
 
 class User(AbstractBaseUser):
+
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+
+    fullname = models.CharField(_('name'), max_length=150, blank=True)
+
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
@@ -45,7 +61,13 @@ class User(AbstractBaseUser):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
 
     def __str__(self):
         return self.email
