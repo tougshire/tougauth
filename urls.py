72aaf32b8ca334ae7525e9b4600ea8b4c129077c
tougshire <benjamin@tougshire.com>
2021-09-11 11:14:00 +0000

    Fixed error in password_reset_confirm url.

--- urls.py
+++ urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('password_change/done/', auth_views.PasswordChangeView.as_view(), name='password_change_done'),
     path('password_reset/', auth_views.PasswordResetView.as_view(),  name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetView.as_view(), name='password_reset_done'),
-    path('reset/<uid64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
