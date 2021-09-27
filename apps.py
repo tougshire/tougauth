dc1853f1bbee2feeb6d9deac572f3aa7948e5c7e
Benjamin Goldberg <bnmng@64goldberg.com>
2021-09-27 11:47:15 +0000

    Simplified this app greatly

--- apps.py
+++ apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TougAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tougauth'
+    verbose_name = 'User Administration'
