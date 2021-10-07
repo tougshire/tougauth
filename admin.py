6ac01b6ffffd6ba97c25f5e45d2fce0e53eab633
Tougshire <tougshire@tougshire.com>
2021-10-07 19:13:05 +0000

    Registerd group with group admin

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from .models import User, Group
 from django.contrib.auth.models import Group as ContribGroup
 
@@ -7,5 +7,5 @@ admin.site.register(User, UserAdmin)
 
 admin.site.unregister(ContribGroup)
 
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
 
--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='tougauth/login.html'), name='login', ),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(),  name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeView.as_view(), name='password_change_done'),
     path('password_reset/', auth_views.PasswordResetView.as_view(),  name='password_reset'),
