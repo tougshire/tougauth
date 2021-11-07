dbe040e583aa20796009a3403df07478bb9e287e
tougshire <87095019+tougshire@users.noreply.github.com>
2021-11-07 08:48:54 -0500

    Added Display Name, Renamed to TougshireAuth

--- models.py
+++ models.py
@@ -2,23 +2,25 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.utils.translation import gettext_lazy as _
 
-class SuvapuliGroup(Group):
+class TougshireAuthGroup(Group):
     class Meta:
-        app_label = 'suvapuli_app'
+        app_label = 'tougshire_auth'
         verbose_name = 'group'
 
-class SuvapuliUser(AbstractUser):
+class TougshireAuthUser(AbstractUser):
     display_name = models.CharField(
-        'display name',
+        'display_name',
         max_length=50,
         blank=True,
-        help_text='A display name, or nickname, if different from firstname lastname'
+        help_text='A display name, or nickname, for example something that would follow "Hello, "'
     )
 
-    def __str__(self):
+    def get_display_name(self):
         if self.display_name > '':
             return self.display_name
-        elif(self.first_name + self.last_name > ''):
+        elif self.first_name + self.last_name > '':
             return f'{self.first_name} {self.last_name}'
         else:
             return self.username
+
+    get_display_name.short_description = 'name'
