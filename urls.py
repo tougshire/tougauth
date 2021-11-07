5fcad46288ff04240f4043135302b5bad0363437
tougshire <87095019+tougshire@users.noreply.github.com>
2021-11-07 08:50:14 -0500

    Added Display Name, Renamed to TougshireAuth

--- urls.py
+++ urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='tougauth/login.html'), name='login', ),
+    path('login/', auth_views.LoginView.as_view(), name='login', ),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(),  name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeView.as_view(), name='password_change_done'),
