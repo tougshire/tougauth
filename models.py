7f36164d4b076adae1458a7a3e8d12707600f2b8
tougshire <87095019+tougshire@users.noreply.github.com>
2021-11-07 07:20:41 -0500

    Reworked & Added Display Name

--- models.py
+++ models.py
@@ -1,7 +1,24 @@
-from django.contrib.auth.models import AbstractUser, Group as ContribGroup
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.utils.translation import gettext_lazy as _
 
-class User(AbstractUser):
-    pass
+class SuvapuliGroup(Group):
+    class Meta:
+        app_label = 'suvapuli_app'
+        verbose_name = 'group'
 
-class Group(ContribGroup):
-    pass
+class SuvapuliUser(AbstractUser):
+    display_name = models.CharField(
+        'display name',
+        max_length=50,
+        blank=True,
+        help_text='A display name, or nickname, if different from firstname lastname'
+    )
+
+    def __str__(self):
+        if self.display_name > '':
+            return self.display_name
+        elif(self.first_name + self.last_name > ''):
+            return f'{self.first_name} {self.last_name}'
+        else:
+            return self.username
