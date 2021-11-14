33f1a94ae96239515198b493e21c8e6357316dff
tougshire <87095019+tougshire@users.noreply.github.com>
2021-11-14 17:46:24 -0500

    Updated Templates

--- urls.py
+++ urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='tougshire_auth/login.html'), name='login', ),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('profile/', views.ProfileDetail.as_view(), name='tougshire_user_profile'),
+    path('profile/update', views.ProfileUpdate.as_view(), name='tougshire_user_profile_update'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='tougshire_auth/password_change_form.html'),  name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeView.as_view(template_name='tougshire_auth/tougshire_auth/password_change_done.html'), name='password_change_done'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='tougshire_auth/password_reset_form.html'),  name='password_reset'),
--- views.py
+++ views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.views.generic.edit import UpdateView
+from django.views.generic.detail import DetailView
+from .models import TougshireAuthUser
 
+class ProfileDetail(DetailView):
+    model = TougshireAuthUser
+    template_name = 'tougshire_auth/profile_detail.html'
+
+    def get_object(self):
+        return self.request.user
+
+class ProfileUpdate(UpdateView):
+    model = TougshireAuthUser
+    template_name = 'tougshire_auth/profile_form.html'
+    fields=['username']
+    def get_object(self):
+        return self.request.user
