740aad3a61ab36f16385d6a9041cfa0363b651b4
tougshire <tougshire@tougshire.com>
2022-01-17 07:47:52 -0500

    Add staff and superuser to list display

--- admin.py
+++ admin.py
@@ -15,7 +15,7 @@ class TougshireAuthUserAdmin(UserAdmin):
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-    list_display=['name', 'username', 'email']
+    list_display=['name', 'username', 'email', 'is_staff', 'is_superuser']
     search_fields = ('username', 'first_name', 'last_name', 'display_name', 'email')
 
 admin.site.register(TougshireAuthUser, TougshireAuthUserAdmin)
