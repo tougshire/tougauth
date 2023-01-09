41848a2726e2c2782a9d2421f3547b8d49b4a6ed
tougshire <tougshire@tougshire.com>
2023-01-09 12:18:16 -0500

    fix duped "tougshire_auth" in ..change_done path

--- urls.py
+++ urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('profile/update', views.ProfileUpdate.as_view(), name='tougshire_user_profile_update'),
     path('profile/update', views.ProfileUpdate.as_view(), name='profile'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='tougshire_auth/password_change_form.html'),  name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeView.as_view(template_name='tougshire_auth/tougshire_auth/password_change_done.html'), name='password_change_done'),
+    path('password_change/done/', auth_views.PasswordChangeView.as_view(template_name='tougshire_auth/password_change_done.html'), name='password_change_done'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='tougshire_auth/password_reset_form.html'),  name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetView.as_view(template_name='tougshire_auth/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='tougshire_auth/password_reset_confirm.html'), name='password_reset_confirm'),
