12d6d973c701c017a70ed65de64195cab87754df
tougshire <tougshire@tougshire.com>
2023-11-04 06:08:36 -0400

    Reformat code done by automatic formatters

--- context_processors.py
+++ context_processors.py
@@ -1,7 +1,5 @@
 from django.conf import settings
 
-def tougshire_auth(request):
-    return {
-        "tougshire_auth": settings.TOUGSHIRE_AUTH
-    }
 
+def tougshire_auth(request):
+    return {"tougshire_auth": settings.TOUGSHIRE_AUTH}
